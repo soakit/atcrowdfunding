5de7dddd7d095cef574cb9ab6fda715cadb817b6
soakit <liuchang2024@gmail.com>
2023-03-22 21:42:16 +0800

    feat: Spring整合MyBatis

---
+++ atcrowdfunding01-admin-parent/atcrowdfunding02-admin-webui/src/main/java/com/atguige/crowd/CrowdTest.java
@@ -0,0 +1,37 @@
+package com.atguige.crowd;
+
+import com.atguigu.crowd.entity.Admin;
+import com.atguigu.crowd.mapper.AdminMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+// 这里用junit5进行测试
+@SpringJUnitConfig(locations = "classpath:spring-persist-mybatis.xml")
+public class CrowdTest {
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private AdminMapper adminMapper;
+
+    @Test
+    public void testInsertAdmin() {
+        Admin admin;
+        admin = new Admin(null, "Rachel", "123123", "rui", "rui@qq.com", null);
+        int count = adminMapper.insert(admin);
+        System.out.println(count);
+    }
+
+    @Test
+    public void testConnection() throws SQLException {
+        Connection conn = dataSource.getConnection();
+        System.out.println(conn);
+    }
+
+}
